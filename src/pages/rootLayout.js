34a2ac17e4d6e2bfa009fe35245b9c50acf7a3b5
HossamMahmoudKhedr <nhem318@gmail.com>
2023-11-12 11:04:37 +0200

    adding first files

---
+++ src/pages/rootLayout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/navbar';
+
+const RootLayout = () => {
+	return (
+		<div className="App">
+			<Navbar />
+
+			<Outlet />
+		</div>
+	);
+};
+
+export default RootLayout;
